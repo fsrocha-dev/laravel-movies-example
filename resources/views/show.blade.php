66cd89e3dd2bb038b8ef666e6fcd394f1d71caa4
fsrocha-dev <contato.frankrocha@gmail.com>
2020-06-06 22:10:05 -0300

    base show movie single blade

---
+++ resources/views/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="movie-info border-b border-gray-800">
+    <div class="container mx-auto px-4 py-16 flex">
+        <img src="/img/starwars.jpg" alt="StarWars" class="w-96" style="width: 24rem">
+        <div class="ml-24">
+            Details here
+        </div>
+    </div>
+</div>
+@endsection
