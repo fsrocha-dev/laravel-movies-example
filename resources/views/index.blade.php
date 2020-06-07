532190b8656e84f49315856f1bd679af4a74b571
fsrocha-dev <contato.frankrocha@gmail.com>
2020-06-06 22:03:52 -0300

    add subtitle tags film

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
         <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
             Popular Movies
         </h2>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
             <div class="mt-8">
                 <a href="#">
                     <img src="/img/starwars.jpg" alt="StarWars"
@@ -25,6 +25,9 @@
                         <span class="ml-1">85%</span>
                         <span class="mx-2">|</span>
                         <span>Feb 20, 2020</span>
+                    </div>
+                    <div class="text-gray-400 text-sm">
+                        Action, Thriller, Comedy
                     </div>
                 </div>
             </div>
